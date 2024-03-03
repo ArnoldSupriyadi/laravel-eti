4d36a80d8097a78df3c6016218916d35141a37ad
Arnold Supriyadi <arnoldsupriyadi@gmail.com>
2024-03-03 14:53:22 +0700

    add image for lang

--- resources/views/layout/frontend/languagehome.blade.php
+++ resources/views/layout/frontend/languagehome.blade.php
@@ -2,10 +2,10 @@
     language
     <ul>
         <li>
-        <a href="index-1.html" class="cursor-link"><img src="img/icons8-indonesia-96.png" alt="indonesia" class="img-fluid" /></a>
+        <a href="index-1.html" class="cursor-link"><img src="{{ asset('frontend/img/icons8-indonesia-96.png') }}" alt="indonesia" class="img-fluid" /></a>
         </li>
         <li>
-        <a href="index-1.html" class="cursor-link"><img src="img/icons8-great-britain-96.png" alt="indonesia" class="img-fluid" /></a>
+        <a href="index-1.html" class="cursor-link"><img src="{{ asset('frontend/img/icons8-great-britain-96.png') }}" alt="indonesia" class="img-fluid" /></a>
         </li>
     </ul>
 </div>
