e0ff2c687f9207c41c8df887d31b1b5e4fc4b765
anandanajahudin <ahmadananda96@gmail.com>
2024-03-18 05:09:56 +0700

    update client seeder

--- database/seeders/ClientSeeder.php
+++ database/seeders/ClientSeeder.php
@@ -12,62 +12,56 @@ class ClientSeeder extends Seeder
     public function run(): void
     {
         DB::table('clients')->insert([
-            'name' => 'KKS',
+            'name' => 'GTP Development',
             'logo' => 'client1.png',
             'created_at' => Carbon::now(),
         ]);
 
         DB::table('clients')->insert([
-            'name' => 'GTP Development',
+            'name' => 'Tekno Solusi Digital Solution',
             'logo' => 'client2.png',
             'created_at' => Carbon::now(),
         ]);
 
         DB::table('clients')->insert([
-            'name' => 'Tekno Solusi Digital Solution',
+            'name' => 'Sun East',
             'logo' => 'client3.png',
             'created_at' => Carbon::now(),
         ]);
 
         DB::table('clients')->insert([
-            'name' => 'Sun East',
+            'name' => 'Tisas',
             'logo' => 'client4.png',
             'created_at' => Carbon::now(),
         ]);
 
         DB::table('clients')->insert([
-            'name' => 'Tisas',
+            'name' => 'Ata Arms',
             'logo' => 'client5.png',
             'created_at' => Carbon::now(),
         ]);
 
         DB::table('clients')->insert([
-            'name' => 'Ata Arms',
+            'name' => '4D Virtualiz',
             'logo' => 'client6.png',
             'created_at' => Carbon::now(),
         ]);
 
         DB::table('clients')->insert([
-            'name' => '4D Virtualiz',
+            'name' => 'Safariland',
             'logo' => 'client7.png',
             'created_at' => Carbon::now(),
         ]);
 
         DB::table('clients')->insert([
-            'name' => 'Safariland',
+            'name' => 'Condor',
             'logo' => 'client8.png',
             'created_at' => Carbon::now(),
         ]);
 
         DB::table('clients')->insert([
-            'name' => 'Condor',
-            'logo' => 'client9.png',
-            'created_at' => Carbon::now(),
-        ]);
-
-        DB::table('clients')->insert([
             'name' => 'Hisense',
-            'logo' => 'client10.png',
+            'logo' => 'client9.png',
             'created_at' => Carbon::now(),
         ]);
     }
