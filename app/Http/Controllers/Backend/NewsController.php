e785fe65e1736dbcafd14846a8f11b994e588940
anandanajahudin <ahmadananda96@gmail.com>
2024-05-11 00:11:01 +0700

    add crud news and portfolio

---
+++ app/Http/Controllers/Backend/NewsController.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use App\Models\News;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\File;
+
+class NewsController extends Controller
+{
+    public function index()
+    {
+        $news = News::orderBy('name', 'ASC')->get();
+
+        return view('pages.backend.news.index', ['news' => $news]);
+    }
+
+    public function create()
+    {
+        return view('pages.backend.news.create');
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required',
+            'description' => 'nullable',
+            'image' => 'nullable|image|mimes:jpeg,png,jpg|max:1024',
+        ]);
+
+        News::create([
+            'name' => $request->name,
+            'description' => $request->description,
+        ]);
+
+        if($request->hasFile('image')) {
+            $idImage = News::latest()->first()->id;
+
+            $datime = date('Y-m-d_H-i-s');
+            $fileName = 'news'.$datime.'.'.$request->image->extension();
+            $destinationPath = 'frontend/img/news/'.$idImage;
+
+            DB::table('news')
+                ->where('id', $idImage)
+                ->update(['image' => $fileName]);
+
+            $request->image->move(public_path($destinationPath), $fileName);
+        }
+
+        return redirect()->route('news.index')->with(['success' => 'News berhasil disimpan!']);
+    }
+
+    public function edit(string $id)
+    {
+        $news = News::findOrFail($id);
+
+        return view('pages.backend.news.edit', compact('news'));
+    }
+
+    public function editImage(string $id)
+    {
+        $news = News::findOrFail($id);
+
+        return view('pages.backend.news.editImage', compact('news'));
+    }
+
+    public function update(Request $request, string $id)
+    {
+        $this->validate($request, [
+            'name' => 'required',
+            'description' => 'nullable',
+        ]);
+
+        $news = News::findOrFail($id);
+
+        $news->update([
+            'name' => $request->name,
+            'description' => $request->description,
+        ]);
+
+        return redirect()->route('news.index')->with(['success' => 'News has been updated!']);
+    }
+
+    public function updateImage(Request $request, string $id)
+    {
+        $this->validate($request, [
+            'image' => 'required|image|mimes:jpeg,png,jpg,svg,webp|max:1024',
+        ]);
+
+        // baru
+        $datime = date('Y-m-d_H-i-s');
+        $fileName = 'news'.$datime.'.'.$request->image->extension();
+        $news = News::findOrFail($id);
+
+        $destinationPath = 'front/img/news/'.$id;
+        $existingFilePath = public_path($destinationPath.'/'.$news->image);
+
+        if (File::exists($existingFilePath)) {
+            // Delete the existing file
+            File::delete($existingFilePath);
+        }
+
+        $news->update(['image' => $fileName]);
+        $request->image->move(public_path($destinationPath), $fileName);
+
+        return redirect()->route('news.index')->with(['success' => 'News Image has been updated!']);
+    }
+
+    public function destroy(string $id, Request $request)
+    {
+        $news = News::findOrFail($id);
+        $news->delete();
+
+        return redirect()->route('news.index')->with(['success' => 'Data berhasil dihapus!']);
+    }
+}
---
+++ app/Http/Controllers/Backend/PortfolioController.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use App\Models\Portfolio;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\File;
+
+class PortfolioController extends Controller
+{
+    public function index()
+    {
+        $portfolios = Portfolio::orderBy('name', 'ASC')->get();
+
+        return view('pages.backend.portfolio.index', ['portfolios' => $portfolios]);
+    }
+
+    public function create()
+    {
+        return view('pages.backend.portfolio.create');
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required',
+            'description' => 'nullable',
+            'description_two' => 'nullable',
+            'image' => 'nullable|image|mimes:jpeg,png,jpg|max:1024',
+        ]);
+
+        Portfolio::create([
+            'name' => $request->name,
+            'description' => $request->description,
+            'description_two' => $request->description_two,
+        ]);
+
+        if($request->hasFile('image')) {
+            $idImage = Portfolio::latest()->first()->id;
+
+            $datime = date('Y-m-d_H-i-s');
+            $fileName = 'portfolio'.$datime.'.'.$request->image->extension();
+            $destinationPath = 'frontend/img/portfolio/'.$idImage;
+
+            DB::table('portfolios')
+                ->where('id', $idImage)
+                ->update(['image' => $fileName]);
+
+            $request->image->move(public_path($destinationPath), $fileName);
+        }
+
+        return redirect()->route('portfolio.index')->with(['success' => 'Portfolio berhasil disimpan!']);
+    }
+
+    public function edit(string $id)
+    {
+        $portfolio = Portfolio::findOrFail($id);
+
+        return view('pages.backend.portfolio.edit', compact('portfolio'));
+    }
+
+    public function editImage(string $id)
+    {
+        $portfolio = Portfolio::findOrFail($id);
+
+        return view('pages.backend.portfolio.editImage', compact('portfolio'));
+    }
+
+    public function update(Request $request, string $id)
+    {
+        $this->validate($request, [
+            'name' => 'required',
+            'description' => 'nullable',
+            'description_two' => 'nullable',
+        ]);
+
+        $portfolio = Portfolio::findOrFail($id);
+
+        $portfolio->update([
+            'name' => $request->name,
+            'description' => $request->description,
+            'description_two' => $request->description_two,
+        ]);
+
+        return redirect()->route('portfolio.index')->with(['success' => 'Portfolio has been updated!']);
+    }
+
+    public function updateImage(Request $request, string $id)
+    {
+        $this->validate($request, [
+            'image' => 'required|image|mimes:jpeg,png,jpg|max:1024',
+        ]);
+
+        // baru
+        $datime = date('Y-m-d_H-i-s');
+        $fileName = 'portfolio'.$datime.'.'.$request->image->extension();
+        $portfolio = Portfolio::findOrFail($id);
+
+        $destinationPath = 'front/img/portfolio/'.$id;
+        $existingFilePath = public_path($destinationPath.'/'.$portfolio->image);
+
+        if (File::exists($existingFilePath)) {
+            // Delete the existing file
+            File::delete($existingFilePath);
+        }
+
+        $portfolio->update(['image' => $fileName]);
+        $request->image->move(public_path($destinationPath), $fileName);
+
+        return redirect()->route('portfolio.index')->with(['success' => 'Portfolio has been updated!']);
+    }
+
+    public function destroy(string $id, Request $request)
+    {
+        $portfolio = Portfolio::findOrFail($id);
+        $portfolio->delete();
+
+        return redirect()->route('portfolio.index')->with(['success' => 'Portfolio berhasil dihapus!']);
+    }
+}
