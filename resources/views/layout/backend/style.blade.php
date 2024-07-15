4dc82976de90845eff22962ff4254970f8f1b6b9
anandanajahudin <ahmadananda96@gmail.com>
2024-07-15 20:26:35 +0700

    update favicon

--- resources/views/layout/backend/style.blade.php
+++ resources/views/layout/backend/style.blade.php
@@ -1,5 +1,12 @@
-<link rel="apple-touch-icon" href="{{ asset('frontend/img/apple-touch-icon.png') }}">
-<link rel="shortcut icon" type="image/x-icon" href="{{ asset('frontend/img/favicon.ico') }}">
+<!-- Favicons  -->
+<link rel="apple-touch-icon" sizes="180x180" href="{{ asset('frontend/apple-touch-icon.png') }}" />
+<link rel="icon" type="image/png" sizes="32x32" href="{{ asset('frontend/favicon-32x32.png') }}" />
+<link rel="icon" type="image/png" sizes="16x16" href="{{ asset('frontend/favicon-16x16.png') }}" />
+<link rel="manifest" href="{{ asset('frontend/site.webmanifest') }}" />
+{{-- <link rel="apple-touch-icon" href="{{ asset('frontend/img/apple-touch-icon.png') }}">
+<link rel="shortcut icon" type="image/x-icon" href="{{ asset('frontend/img/favicon.ico') }}"> --}}
+
+<!-- Fonts  -->
 <link
     href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i%7CQuicksand:300,400,500,700"
     rel="stylesheet">
