465b42ed9ab85f1baf7c8c605fdb4fc7c6894091
anandanajahudin <ahmadananda96@gmail.com>
2024-07-15 13:33:54 +0700

    add execute seeder product type

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             ProductSeeder::class,
             ProductCategorySeeder::class,
+            ProductTypeSeeder::class,
         ]);
     }
 }
