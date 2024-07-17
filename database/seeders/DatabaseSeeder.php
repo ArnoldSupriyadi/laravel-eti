3d23d6b706de9d1a51c543579a142b77d18a2424
anandanajahudin <ahmadananda96@gmail.com>
2024-07-17 14:35:14 +0700

    add product category gallery seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,6 +19,7 @@ class DatabaseSeeder extends Seeder
             ProductSeeder::class,
             ProductCategorySeeder::class,
             ProductTypeSeeder::class,
+            ProductCategoryGallerySeeder::class,
         ]);
     }
 }
